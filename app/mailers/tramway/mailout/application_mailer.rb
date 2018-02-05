b83dfca847a4ca047138f1c8f1621b2815646807
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-02-05 18:35:39 +0400

    Fix

--- app/mailers/tramway/mailout/application_mailer.rb
+++ app/mailers/tramway/mailout/application_mailer.rb
@@ -3,7 +3,7 @@ module Tramway
     class ApplicationMailer < ActionMailer::Base
       def just_message(email, title, body)
         @body = body
-        mail from: sender_name, to: email, subject: title
+        mail from: 'Test', to: email, subject: title
       end
     end
   end
