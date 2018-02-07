712c8c409cc3f21cd2b22631d1160f1edd7dda14
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-02-07 15:07:41 +0400

    Fix template path

--- app/mailers/tramway/mailout/application_mailer.rb
+++ app/mailers/tramway/mailout/application_mailer.rb
@@ -1,6 +1,8 @@
 module Tramway
   module Mailout
     class ApplicationMailer < ActionMailer::Base
+      default template_path: 'mailers'
+
       def just_message(email, title, body)
         @body = body
         mail from: 'Test', to: email, subject: title
